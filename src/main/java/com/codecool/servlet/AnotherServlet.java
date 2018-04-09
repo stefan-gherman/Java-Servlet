16991217894f4f09d05bc1774cacf29693bf48ae
Farago Balazs <minimalgeek@gmail.com>
2018-04-09 02:22:52 +0200

    Another servlet implementation with links

---
+++ src/main/java/com/codecool/servlet/AnotherServlet.java
@@ -0,0 +1,41 @@
+package com.codecool.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "anotherServlet", urlPatterns = {"/another"}, loadOnStartup = 2)
+public class AnotherServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+
+        StringBuffer buffer = new StringBuffer();
+        for (int i = 0; i < 10; i++) {
+            buffer.append("<div>");
+            buffer.append("<a href=\"/another?link_id=" + i + "\">");
+            buffer.append("Hello " + i + ". link:");
+            buffer.append("</a>");
+            buffer.append("</div>");
+        }
+
+        String linkId = request.getParameter("link_id");
+
+        out.println(
+                "<html>\n" +
+                        "<head><title>Another page</title></head>\n" +
+                        "<body>\n" +
+                        "<h1>Happy coding!</h1>" +
+                        (linkId == null ?
+                                "<h3>No link was pressed</h3>" :
+                                "<h3>Link " + linkId + " was pressed!</h3>") +
+                        "<br/>" +
+                        "<div>" + buffer.toString() + "</div>" +
+                        "</body></html>"
+        );
+    }
+}
--- src/main/java/com/codecool/servlet/ServletWithAnnotations.java
+++ src/main/java/com/codecool/servlet/ServletWithAnnotations.java
@@ -1,22 +1,36 @@
 package com.codecool.servlet;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "simpleServlet", urlPatterns = { "/hello" }, loadOnStartup = 1)
+@WebServlet(name = "simpleServlet", urlPatterns = {"/"}, loadOnStartup = 1)
 public class ServletWithAnnotations extends HttpServlet {
 
-	private static final long serialVersionUID = -3462096228274971485L;
+    private static final long serialVersionUID = -3462096228274971485L;
 
-	@Override
-	protected void doGet(HttpServletRequest reqest, HttpServletResponse response) 
-			throws ServletException, IOException {
-		response.getWriter().println("Hello World!");
-	}
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+        PrintWriter out = response.getWriter();
+        String title = "Using GET Method to Read Form Data";
+
+        out.println(
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body>\n" +
+                "<h1 align = \"center\">" + title + "</h1>\n" +
+                "<ul>\n" +
+                "<li><b>First Name</b>: " + request.getParameter("first_name") + "\n" +
+                "<li><b>Last Name</b>: " + request.getParameter("last_name") + "\n" +
+                "</ul>\n" +
+                "<div>Visit another servlet: <a href=\"/another\">Visit the other servlet</a></div>" +
+                "</body></html>"
+        );
+    }
 }
