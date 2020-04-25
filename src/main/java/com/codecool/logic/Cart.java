1278cf1e3295ea40ef2fc62fedbc11056cfd5849
Stefan Gherman <scgheman@gmail.com>
2020-04-26 02:59:55 +0300

    Add cart class

--- src/main/java/com/codecool/logic/Cart.java
+++ src/main/java/com/codecool/logic/Cart.java
@@ -1,4 +1,34 @@
 package com.codecool.logic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
+
+    private List<Item> itemsList;
+
+    public Cart() {
+        this.itemsList = new ArrayList<Item>();
+    }
+
+    public void addItemToCart(Item item) {
+        this.itemsList.add(item);
+    }
+
+    public void removeItemFromCart(Item item) {
+        if(itemsList.contains(item)) {
+            itemsList.remove(item);
+        }
+    }
+
+    public List<Item> getItemsList() {
+        return itemsList;
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "itemsList=" + itemsList +
+                '}';
+    }
 }
